5672131f6787a517c4e87753ee48f7a43d68c2d9
Trevor <85640211+trevordevelops@users.noreply.github.com>
2022-02-14 14:04:26 -0800

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Trevor Welsh on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
